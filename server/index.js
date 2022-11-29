9ea3b3e32e312e45d94adf1a9ccb522b31098cce
Giga Uchaneishvili <g.uchaneishvili@gmail.com>
2022-11-29 23:23:22 +0400

    fix

--- server/index.js
+++ server/index.js
@@ -1,101 +1,231 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
-const todoTasks = require("./models/todoTasks");
+/** @format */
 
-var cors = require("cors");
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const todoTasks = require('./models/todoTasks');
+
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+var cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
 
 mongoose.connect(
-  "mongodb+srv://guchaneishvili:Gig@20003030@cluster0.p6fos.mongodb.net/todoApp?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-  }
+	'mongodb+srv://guchaneishvili:Gig@20003030@cluster0.p6fos.mongodb.net/todoApp?retryWrites=true&w=majority',
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}
 );
 
-app.get("/read", async (req, res) => {
-  try {
-    let query = todoTasks.find({});
+const swaggerOption = {
+	swagger: '2.0',
+	swaggerDefinition: {
+		info: {
+			title: 'Todo API',
+			description: 'Todo API information',
+			contact: {
+				name: 'Giga Uchaneishvili',
+			},
+			servers: ['http://localhost:3001'],
+		},
+	},
 
-    const page = parseInt(req.query.page) || 1;
-    const pageSize = parseInt(req.query.pageSize) || 5;
-    const skip = (page - 1) * pageSize;
-    const total = await todoTasks.countDocuments();
+	// ['.routes/*js]
+	apis: ['index.js'],
+};
 
-    const pages = Math.ceil(total / pageSize);
+const swaggerDocs = swaggerJsDoc(swaggerOption);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-    query = query.skip(skip).limit(pageSize);
-    const result = await query;
+//Routes
 
-    res.status(200).json({
-      status: "Success",
-      count: result.length,
-      page,
-      pages,
-      data: result,
-    });
-  } catch (error) {
-    console.log(error);
+/**
+ * @swagger
+ * /read:
+ *  get:
+ *    summary: Get all todo list.
+ *    responses:
+ *      '200':
+ *        description: A successfull response
+ *
+ */
+app.get('/read', async (req, res) => {
+	try {
+		let query = todoTasks.find({});
 
-    res.status(500).json({
-      status: "Failed",
-      message: "Server Error 🆘 ",
-    });
-  }
+		const page = parseInt(req.query.page) || 1;
+		const pageSize = parseInt(req.query.pageSize) || 5;
+		const skip = (page - 1) * pageSize;
+		const total = await todoTasks.countDocuments();
+
+		const pages = Math.ceil(total / pageSize);
+
+		query = query.skip(skip).limit(pageSize);
+		const result = await query;
+
+		res.status(200).json({
+			status: 'Success',
+			count: result.length,
+			page,
+			pages,
+			data: result,
+		});
+		
+	} catch (error) {
+		console.log(error);
+
+		res.status(500).json({
+			status: 'Failed',
+			message: 'Server Error 🆘 ',
+		});
+	}
 });
 
-app.get("/read/:id", async (req, res) => {
-  const id = req.params.id;
+/**
+ *  @swagger
+ *
+ * paths:
+ *   /read/{id}:
+ *     get:
+ *       summary: Get specific todo task info.
+ *       consumes:
+ *         - application/json
+ *       parameters:
+ *         - in: path
+ *           name: id
+ *           schema:
+ *           type: string
+ *           required: true
+ *           description: id of todo task
+ *       responses:
+ *         201:
+ *           description: OK
+ */
+app.get('/read/:id', async (req, res) => {
+	const id = req.params.id;
 
-  todoTasks.findById(id, (err, result) => {
-    if (err) {
-      res.send(err);
-    }
+	todoTasks.findById(id, (err, result) => {
+		if (err) {
+			res.send(err);
+		}
 
-    res.send(result);
-  });
+		res.send(result);
+	});
 });
 
-app.post("/insert", async (req, res) => {
-  const { name } = req.body;
+/**
+ *  @swagger
+ *
+ * paths:
+ *   /insert:
+ *     post:
+ *       summary: Creates a new Todo.
+ *       consumes:
+ *         - application/json
+ *       parameters:
+ *         - in: body
+ *           name: Todo
+ *           description: Creates a new  Todo task.
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *       responses:
+ *         200:
+ *           description: Created
+ */
+app.post('/insert', async (req, res) => {
+	const { name } = req.body;
 
-  const task = new todoTasks({
-    name: name,
-    overline: false,
-  });
-  try {
-    await task.save();
-    res.send("INSERTED DATA ‼️");
-  } catch (error) {
-    console.log(error);
-    res.send(error);
-  }
+	const task = new todoTasks({
+		name: name,
+		overline: false,
+	});
+	try {
+		await task.save();
+		res.send(task);
+	} catch (error) {
+		console.log(error);
+		res.send(error);
+	}
 });
 
-app.put("/update", async (req, res) => {
-  const { name, overline, _id } = req.body;
+/**
+ *  @swagger
+ *
+ * paths:
+ *   /update/{id}:
+ *     put:
+ *       summary: update a Todo task.
+ *       consumes:
+ *         - application/json
+ *       parameters:
+ *         - in: path
+ *           name: id
+ *           type: string
+ *           required: true
+ *           description: id of task
+ *         - in: body
+ *           name: Todo
+ *           description: update a Todo task.
+ *           schema:
+ *             type: object
+ *             properties:
+ *               overline:
+ *                 type: boolean
+ *       responses:
+ *         200:
+ *           description: updated
+ */
+app.put('/update/:id', async (req, res) => {
+	const { overline } = req.body;
+	const { id } = req.params;
 
-  try {
-    const updateTask = await todoTasks.findById(_id);
-    updateTask.name = name;
-    updateTask.overline = overline;
+	try {
+		const updateTask = await todoTasks.findByIdAndUpdate(id, {
+			overline: overline,
+		});
 
-    updateTask.save();
+		updateTask.save();
 
-    res.send("update tasks");
-  } catch (error) {
-    console.log(error);
-  }
+		res.send(updateTask);
+	} catch (error) {
+		console.log(error);
+	}
 });
-app.delete("/delete/:id", async (req, res) => {
-  const id = req.params.id;
-  await todoTasks.findByIdAndRemove(id).exec();
 
-  res.send("deleted !♻️");
+/**
+ *  @swagger
+ *
+ * paths:
+ *   /delete/{id}:
+ *     delete:
+ *       summary: Delete a todo task.
+ *       consumes:
+ *         - application/json
+ *       parameters:
+ *         - in: path
+ *           name: id
+ *           schema:
+ *           type: string
+ *           required: true
+ *           description: id of task
+ *       responses:
+ *         201:
+ *           description: OK
+ */
+app.delete('/delete/:id', async (req, res) => {
+	const id = req.params.id;
+	await todoTasks.findByIdAndRemove(id).exec();
+
+	res.send('deleted !♻️');
 });
 
 app.listen(3001, () => {
-  console.log("🚀 Server running on port 3001... ");
+	console.log('🚀 Server running on port 3001... ');
 });
--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,167 +1,159 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import "./App.css";
+/** @format */
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
-  const [tasksList, setTasksList] = useState([]);
-  const [inputValue, setInputValue] = useState("");
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState();
+	const [tasksList, setTasksList] = useState([]);
+	const [inputValue, setInputValue] = useState('');
+	const [currentPage, setCurrentPage] = useState(1);
+	const [totalPages, setTotalPages] = useState();
 
-  useEffect(() => {
-    loadData(1);
-  }, []);
+	useEffect(() => {
+		loadData(currentPage);
+	}, [currentPage]);
 
-  useEffect(() => {
-    loadData(currentPage);
-  }, [currentPage]);
+	const selectAsComplete = (task) => {
+		task.overline = !task.overline;
+		addOrUpdateTask(task);
+	};
 
-  const selectAsComplete = (task) => {
-    console.log(task);
-    task.overline = !task.overline;
-    addOrUpdateTask(task);
-  };
+	const addOrUpdateTask = async (task) => {
+		if (task && task._id) {
+			await axios.put(`http://localhost:3001/update/${task._id}`, task);
+		} else {
+			await axios.post('http://localhost:3001/insert', {
+				name: inputValue,
+				overline: false,
+			});
+		}
+		setInputValue('');
+		loadData(currentPage);
+	};
 
-  const addOrUpdateTask = async (task) => {
-    if (task && task._id) {
-      await axios.put("http://localhost:3001/update", task);
-    } else {
-      await axios.post("http://localhost:3001/insert", {
-        name: inputValue,
-        overline: false,
-      });
-    }
-    setInputValue("");
-    loadData(currentPage);
-  };
+	const userInput = (event) => {
+		setInputValue(event.target.value);
+	};
 
-  const userInput = (event) => {
-    setInputValue(event.target.value);
-  };
+	const loadData = async (page) => {
+		setTasksList([]);
+		let url = `http://localhost:3001/read?page=${page}`;
 
-  const loadData = async (page) => {
-    setTasksList([]);
-    let url = `http://localhost:3001/read?page=${page}`;
+		await axios.get(url).then((response) => {
+			setTasksList(response.data.data);
+			setTotalPages(response.data.pages);
+		});
+	};
 
-    await axios.get(url).then((response) => {
-      setTasksList(response.data.data);
-      setTotalPages(response.data.pages);
-    });
-  };
+	const deleteTask = async (id) => {
+		await axios.delete(`http://localhost:3001/delete/${id}`);
+		loadData(currentPage);
+		setCurrentPage(1);
+		setInputValue('');
+	};
 
-  const deleteTask = async (id) => {
-    await axios.delete(`http://localhost:3001/delete/${id}`);
-    loadData(currentPage);
-    setInputValue("");
-  };
+	const nextPage = () => {
+		setCurrentPage(currentPage + 1);
+	};
 
-  const nextPage = () => {
-    setCurrentPage(currentPage + 1);
-  };
+	const previousPage = () => {
+		setCurrentPage(currentPage - 1);
+	};
 
-  const previousPage = () => {
-    setCurrentPage(currentPage - 1);
-  };
+	const generateTable = () => {
+		const table = [];
 
-  const generateTable = () => {
-    const table = [];
+		for (let i = 0; i < tasksList.length; i++) {
+			table.push(
+				<tr key={tasksList[i]._id}>
+					<td className={tasksList[i].overline ? 'is-complete' : ''}>
+						{5 * (currentPage - 1) + (i + 1)}
+					</td>
+					<td className={tasksList[i].overline ? 'is-complete' : ''}>
+						{tasksList[i].name}
+					</td>
+					<td className='buttons-container'>
+						<button
+							type='button'
+							className='btn btn-danger delete-button'
+							onClick={() => deleteTask(tasksList[i]._id)}>
+							<ion-icon name='trash-outline'></ion-icon>
+						</button>
 
-    for (let i = 0; i < tasksList.length; i++) {
-      table.push(
-        <tr key={tasksList[i]._id}>
-          <td className={tasksList[i].overline ? "is-complete" : ""}>
-            {5 * (currentPage - 1) + (i + 1)}
-          </td>
-          <td className={tasksList[i].overline ? "is-complete" : ""}>
-            {tasksList[i].name}
-          </td>
-          <td className="buttons-container">
-            <button
-              type="button"
-              className="btn btn-danger delete-button"
-              onClick={() => deleteTask(tasksList[i]._id)}
-            >
-              <ion-icon name="trash-outline"></ion-icon>
-            </button>
+						<button
+							type='button'
+							className='btn btn-success complete-button'
+							onClick={() => selectAsComplete(tasksList[i])}>
+							<ion-icon name='checkmark-done-outline'></ion-icon>
+						</button>
+					</td>
+				</tr>
+			);
+		}
+		return table;
+	};
 
-            <button
-              type="button"
-              className="btn btn-success complete-button"
-              onClick={() => selectAsComplete(tasksList[i])}
-            >
-              <ion-icon name="checkmark-done-outline"></ion-icon>
-            </button>
-          </td>
-        </tr>
-      );
-    }
-    return table;
-  };
+	return (
+		<div className='container todoMainApp'>
+			<h1 className='headTitle'>Todo App</h1>
+			<div className='input-group'>
+				<input
+					type='text'
+					className='form-control task-input'
+					name='name'
+					value={inputValue}
+					aria-label='Text input with segmented dropdown button'
+					onChange={userInput}
+				/>
 
-  return (
-    <div className="container todoMainApp">
-      <h1 className="headTitle">Todo App</h1>
-      <div className="input-group">
-        <input
-          type="text"
-          className="form-control task-input"
-          name="name"
-          value={inputValue}
-          aria-label="Text input with segmented dropdown button"
-          onChange={userInput}
-        />
+				<button
+					type='submit'
+					className='btn btn-outline-light submit-button'
+					onClick={() => addOrUpdateTask()}>
+					Add Task
+				</button>
+			</div>
+			{tasksList.length ? (
+				<table className='table'>
+					<thead>
+						<tr>
+							<th scope='col'>#</th>
+							<th scope='col'>Name</th>
+							<th scope='col'>Action</th>
+						</tr>
+					</thead>
+					<tbody>{generateTable()}</tbody>
+				</table>
+			) : (
+				' '
+			)}
 
-        <button
-          type="submit"
-          className="btn btn-outline-light submit-button"
-          onClick={() => addOrUpdateTask()}
-        >
-          Add Task
-        </button>
-      </div>
-      {tasksList.length ? (
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Name</th>
-              <th scope="col">Action</th>
-            </tr>
-          </thead>
-          <tbody>{generateTable()}</tbody>
-        </table>
-      ) : (
-        " "
-      )}
-
-      {tasksList.length ? (
-        <div className="pagination-container">
-          <button
-            className="btn btn-outline-light pagination-buttons"
-            disabled={currentPage === 1}
-            onClick={() => previousPage()}
-          >
-            {"<"}
-          </button>
-          <button
-            className="btn btn-outline-light pagination-buttons current-page"
-            disabled
-          >
-            {currentPage}
-          </button>
-          <button
-            className="btn btn-outline-light pagination-buttons"
-            disabled={currentPage === totalPages}
-            onClick={() => nextPage()}
-          >
-            {">"}
-          </button>
-        </div>
-      ) : (
-        " "
-      )}
-    </div>
-  );
+			{tasksList.length ? (
+				<div className='pagination-container'>
+					<button
+						className='btn btn-outline-light pagination-buttons'
+						disabled={currentPage === 1}
+						onClick={() => previousPage()}>
+						{'<'}
+					</button>
+					<button
+						className='btn btn-outline-light pagination-buttons current-page'
+						disabled>
+						{currentPage}
+					</button>
+					<button
+						className='btn btn-outline-light pagination-buttons'
+						disabled={currentPage === totalPages}
+						onClick={() => nextPage()}>
+						{'>'}
+					</button>
+				</div>
+			) : (
+				' '
+			)}
+		</div>
+	);
 }
 
 export default App;
